19c378ff571168040a48e6427110738f93802275
Robb Currall <rlcurrall@gmail.com>
2024-01-31 20:07:20 -0500

    refactor: switch to askama for templating

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,14 +1,13 @@
-use actix_session::{
-    config::PersistentSession, storage::CookieSessionStore, Session, SessionMiddleware,
-};
+use actix_session::{config::PersistentSession, storage::CookieSessionStore, SessionMiddleware};
 use actix_web::{
     cookie::{self, Key},
     middleware::{Logger, NormalizePath},
     web::{scope, Data},
     App, HttpResponse, HttpServer,
 };
+use askama::Template;
 use dotenvy::dotenv;
-use handlebars::{DirectorySourceOptions, Handlebars};
+use tracing_actix_web::TracingLogger;
 
 use daedalus::{
     database::{DatabaseSettings, PoolManager},
@@ -16,36 +15,48 @@ use daedalus::{
     services::{tenants::TenantService, users::UserService, workflows::WorkflowService},
 };
 
-async fn index(hb: Data<Handlebars<'_>>) -> HttpResponse {
-    let body = hb.render("index", &()).unwrap();
+async fn index() -> HttpResponse {
+    let body = HomePage.render().unwrap();
     HttpResponse::Ok().body(body)
+}
+
+#[derive(Clone, Debug)]
+struct AppSettings {
+    _debug: bool,
+    database: DatabaseSettings,
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
+    env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
-    let mut handlebars = Handlebars::new();
-    if cfg!(debug_assertions) {
-        handlebars.set_dev_mode(true);
-    }
-    handlebars
-        .register_templates_directory("./views", DirectorySourceOptions::default())
-        .unwrap();
+    // todo: source this from a file or env vars
+    let app_settings = AppSettings {
+        _debug: cfg!(debug_assertions),
+        database: DatabaseSettings {
+            database_url: std::env::var("DATABASE_URL").expect("DATABASE_URL must be set"),
+            max_connections: None,
+            idle_timeout: None,
+            connection_timeout: None,
+            thread_pool_size: None,
+        },
+    };
 
-    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let pool = PoolManager::new(&DatabaseSettings::new(database_url.clone()));
-    let user_service = UserService::new(pool.get_pool());
-    let workflow_service = WorkflowService::new(pool.get_pool());
-    let tenant_service = TenantService::new(pool.get_pool());
+    let pool_manager = PoolManager::new(&app_settings.database);
+    let user_service = UserService::new(pool_manager.get_pool());
+    let workflow_service = WorkflowService::new(pool_manager.get_pool());
+    let tenant_service = TenantService::new(pool_manager.get_pool());
 
     HttpServer::new(move || {
         App::new()
             .app_data(Data::new(user_service.clone()))
             .app_data(Data::new(tenant_service.clone()))
             .app_data(Data::new(workflow_service.clone()))
+            .app_data(Data::new(app_settings.clone()))
             .wrap(NormalizePath::trim())
             .wrap(Logger::default())
+            .wrap(TracingLogger::default())
             .wrap(
                 SessionMiddleware::builder(CookieSessionStore::default(), Key::from(&[0; 64]))
                     .cookie_secure(false)
@@ -54,12 +65,27 @@ async fn main() -> std::io::Result<()> {
                     )
                     .build(),
             )
-            .service(scope("/api/users").configure(users::configure))
-            .service(scope("/api/tenants").configure(tenants::configure))
-            .service(scope("/api/workflows").configure(workflows::configure))
-            .route("/", actix_web::web::get().to(index))
+            .configure(configure_web)
+            .configure(configure_api)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
     .await
+}
+
+pub fn configure_api(cfg: &mut actix_web::web::ServiceConfig) {
+    cfg.service(
+        scope("/api")
+            .configure(users::configure)
+            .configure(tenants::configure)
+            .configure(workflows::configure),
+    );
+}
+
+#[derive(Template)]
+#[template(path = "pages/index.j2")]
+struct HomePage;
+
+pub fn configure_web(cfg: &mut actix_web::web::ServiceConfig) {
+    cfg.route("/", actix_web::web::get().to(index));
 }
