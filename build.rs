696ec1ac18900bd1a0adbca6057abf1f09cbd9c4
Robb Currall <rlcurrall@gmail.com>
2024-03-01 20:28:58 -0500

    feat: update build script to build ts as well

--- build.rs
+++ build.rs
@@ -1,11 +1,27 @@
-use std::process::Command;
+use std::{fs, process::Command};
 
 fn main() {
-    // taken from https://stackoverflow.com/questions/43753491/include-git-commit-hash-as-string-into-rust-program
+    println!("cargo:rerun-if-changed=resources");
+
+    fs::remove_dir_all("public/build").expect("Could not remove public/build directory");
+
+    let npm_build = Command::new("npm")
+        .args(&["run", "build"])
+        .output()
+        .expect("npm build failed");
+    if !npm_build.status.success() {
+        panic!(
+            "npm build failed: {}",
+            String::from_utf8(npm_build.stderr)
+                .expect("Could not get error message from npm build")
+        );
+    }
+
     let output = Command::new("git")
         .args(&["rev-parse", "HEAD"])
         .output()
-        .unwrap();
+        .expect("git rev-parse HEAD failed");
+
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=BUILD_ID={}", git_hash);
 }
