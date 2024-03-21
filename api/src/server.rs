9835fabc3d4f8be2821d6ce7ee4659d407eec142
Robb Currall <rlcurrall@gmail.com>
2024-03-20 21:13:30 -0400

    feat: register json error handler

--- api/src/server.rs
+++ api/src/server.rs
@@ -1,5 +1,5 @@
 use actix_web::middleware::{Compress, NormalizePath};
-use actix_web::web::Data;
+use actix_web::web::{Data, JsonConfig};
 use actix_web::{App, HttpServer};
 use tracing_actix_web::TracingLogger;
 
@@ -17,6 +17,17 @@ pub async fn start(settings: AppSettings) -> Result<(), Box<dyn std::error::Erro
 
     HttpServer::new(move || {
         App::new()
+            .app_data(JsonConfig::default().error_handler(|err, _req| {
+                let json = serde_json::json!({
+                    "error": "Failed to parse JSON",
+                    "detail": err.to_string()
+                });
+                actix_web::error::InternalError::from_response(
+                    err,
+                    actix_web::HttpResponse::UnprocessableEntity().json(json),
+                )
+                .into()
+            }))
             .app_data(Data::new(settings.clone()))
             .app_data(Data::new(pool_manager.clone()))
             .wrap(NormalizePath::trim())
